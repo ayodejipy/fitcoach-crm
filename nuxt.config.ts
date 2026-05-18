8167594ee2313ebad477cbd3cb4fb58c75142cd3
ayodejipy <ay.programmr@gmail.com>
2026-05-18 14:08:35 +0100

    feat: updated file dependencies

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxt/ui', '@pinia/nuxt'],
+  future: {
+    compatibilityVersion: 4,
+  },
+  runtimeConfig: {
+    public: {
+      // Override in production via NUXT_PUBLIC_API_BASE env var
+      apiBase: 'http://localhost:8080',
+    },
+  },
+})
